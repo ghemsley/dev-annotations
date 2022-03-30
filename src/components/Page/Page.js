0234dd1a562f48d820ad44057725dbabf3c40928
Graham Hemsley <graham.hemsley@teladochealth.com>
2022-03-30 13:54:30 -0500

    Fix axe issue

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -5,7 +5,7 @@ import styles from './Page.module.scss'
 
 function Page() {
   return (
-    <main className={styles.page} aria-role='bagel'>
+    <main className={styles.page}>
       <Title />
       <VisitCard />
     </main>
