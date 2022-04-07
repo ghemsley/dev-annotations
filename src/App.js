1ed7aed4d4beb493db465abe2c4ab90aa3597418
Graham Hemsley <graham.hemsley@teladochealth.com>
2022-04-07 14:08:37 -0500

    Remove wrapper container, update skiplink styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './App.scss'
-import Container from './components/Container'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Page from './components/Page'
@@ -8,12 +7,12 @@ import SkipLink from './components/SkipLink'
 
 function App() {
   return (
-    <Container>
+    <>
       <SkipLink />
       <Header />
       <Page />
       <Footer />
-    </Container>
+    </>
   )
 }
 
